b9b8f37d16c083ea978a2aac8bcfca4621a78393
scr1pt3r <masterscripter@protonmail.com>
2022-04-23 00:51:27 -0300

    recebendo dados da birpreço e imprimindo no Console

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//Declarando variáveis
+const clc = require('cli-color');
+const yellow = clc.yellow;
+const green = clc.green;
+const red = clc.red;
+const blue = clc.blue;
+
 function getJSON(api) {
 	const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 	const request = new XMLHttpRequest();
@@ -5,9 +12,30 @@ function getJSON(api) {
 	request.open('GET', api, false);
 	request.send();
 
-	let responseObj = JSON.parse(request.responseText);
+	let response = JSON.parse(request.responseText);
 
 	return response;
+}
+
+function getBTC() {
+	console.log('\nIniciando requisição na API da BitPreço...');
+
+	const url = "https://api.bitpreco.com/btc-brl/";
+	const ticker = url + "ticker";
+	const book = url + "orderbook";
+	const trades = url + "trades";
+
+	let tickerData = getJSON(ticker);
+	let lastPrice = tickerData.last;
+	let timestamp = tickerData.timestamp;
+	let low24H = tickerData.low;
+	let high24H = tickerData.high;
+
+	console.log(yellow('Resumo do mercado: '));
+	console.log('\nÚltimo preço registrado: ' + parseFloat(lastPrice).toFixed(2));
+	console.log(tickerData);
+	let dados = getJSON(book);
+	console.log('Tamanho: ' + dados.timestamp);
 }
 
 function main() {
@@ -17,3 +45,5 @@ function main() {
 
 	return
 }
+
+main();
